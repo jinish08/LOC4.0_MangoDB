e55872d0df8d14f2d52b8e39c17c7d813b992aa7
jinish08 <jinishshah08@gmailcom>
2022-03-12 15:05:32 +0530

    made the router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
