18184cb4c1aeb181cfae68bc97652521a86b18d8
Jainam Shah <jainams6160@gmail.com>
2022-03-12 17:50:03 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cricket from "./pages/cricket";
+import Exercise1 from "./pages/cricket/Exercise1";
 import Dashboard from "./pages/Dashboard";
 import Landing from "./pages/Landing";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/landing" element={<Landing />} />
+        <Route path="/cricket" element={<Cricket />} />
+        <Route path="cricket/e1" element={<Exercise1 />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/cricket/Exercise1.js
@@ -0,0 +1,20 @@
+import { VStack, HStack, Text } from '@chakra-ui/react';
+import React from 'react'
+
+const Exercise1 = () => {
+  return (
+    <>
+
+        <HStack >
+            <VStack>
+                <Text>Hello</Text>
+            </VStack>
+            <VStack>
+                <Text>Hello</Text>
+            </VStack>
+        </HStack>
+    </>
+  )
+}
+
+export default Exercise1;
